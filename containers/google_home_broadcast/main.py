6068f6554c1dc6c24853cc26ec8fc7dbba0a20ed
Damian Kelly <damian.t.kelly@gmail.com>
2020-06-02 20:06:21 +0100

    Added another dummy camera image and fixed google broadcaster subscription

--- containers/google_home_broadcast/main.py
+++ containers/google_home_broadcast/main.py
@@ -49,7 +49,7 @@ def on_connect(client, userdata, flags, rc):
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("#")  # $SYS/
+    client.subscribe("+/google_home_broadcast/+")  # $SYS/
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
